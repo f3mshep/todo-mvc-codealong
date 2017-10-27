ff778228d5de5a50d02151642d4e7baa96389e76
Alexandra Wright <superbiscuit@gmail.com>
2017-10-27 10:49:40 -0700

    form refactoring complete

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -2,4 +2,11 @@ module ItemsHelper
     def li_class_for_item(item)
         "completed" if item.complete?
     end
+
+    def li_for_item(item)
+        content_tag_for :li, item, :class => li_class_for_item(item) do
+            yield
+        end
+    end
+
 end

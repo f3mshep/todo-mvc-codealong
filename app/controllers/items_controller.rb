f1e2bd168d530b4e941694c930d64225a4e56b9c
Alexandra Wright <superbiscuit@gmail.com>
2017-10-27 10:24:08 -0700

    list item status working

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,7 @@ class ItemsController < ApplicationController
   def create
     @list = List.find(params[:list_id])
     @item = @list.items.build(item_params)
+ 
     if @item.save
       redirect_to list_path(@list)
     else
@@ -10,11 +11,16 @@ class ItemsController < ApplicationController
     end
   end
 
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to list_path(@item.list)
+  end
 
   private
 
   def item_params
-    params.require(:item).permit(:description)
+    params.require(:item).permit(:description, :status)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ActiveRecord::Base
     belongs_to :list
     validates :description, presence: true
+    STATUS = {
+        :complete => 1,
+        :incomplete => 2
+    }
+
+    def complete?
+        self.status == STATUS[:complete]
+    end
+
+    def incomplete?
+        self.status == STATUS[:incomplete]
+    end
+
 end
